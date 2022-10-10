eb1f0bf890314a1fc1d3a7d5ad63d3a4249e3131
saurondk <93879292+saurondk@users.noreply.github.com>
2022-10-10 11:51:43 +0100

    10-10

--- Unidad3/String ej3.2/texto.js
+++ Unidad3/String ej3.2/texto.js
@@ -1,17 +1,86 @@
 var texto ="Cliente;Localidad;Cuota-Laura;Santander;50-Álvaro;Castro;50-Igor;Castro;60-Ivan;Santander;40-Mónica;Zamora;30-Javi;Bilbao;30-David;Bilbao;50-José Luis;Bilbao;60";
 var array1= texto.split("-");
-var array2;
+var tabl="";
+var tablap="";
+var linea=[];
+var arraylimpio=[];
 
-function usuarios(){
-    posicion=0;
-    for (let index = 0; index < array1.length; index++) {
-        array2= (array1[index].split(";"));
-        for (let index = 0; index < array2.length; index++) {
-            document.write(array2[index]);
-            
-        }
+var invisible=' <input type="text"  id="prov" hidden>';
+invisible+='<button onclick="provincias()" hidden>Buscar</button>';
+
+var visible=' <input type="text" id="prov" >';
+visible+='<button onclick="provincias()">Buscar</button>';
+
+for (let index = 0; index < array1.length; index++) {
+     var array2= (array1[index].split(";"));
+    for (let i = 0; i < array2.length; i++) {
+        linea[i]=array2[i];
     }
+   arraylimpio[index]=linea;
+   linea=[];
+}
+function usuarios(){
+    document.getElementById("cajaprov").innerHTML=invisible;
+    var tabl="";
+    tabl += "<p> tabla con usuarios:</p> <table style ='border:1px solid black;'>";
+
+    for (let index = 0; index < arraylimpio.length; index++) {
+        tabl+="<tr>";
+        for (let i = 0; i < arraylimpio[index].length; i++) {
+            tabl+="<td>"+arraylimpio[index][i]+"</td>";  
+        }
+        tabl+="</tr>";
+    }
+    tabl+="</table>";
+    document.getElementById("usuario").innerHTML=tabl;
     
-    //document.getElementById("usuarios").innerHTML=;
+}
+function visi(){
+    document.getElementById("cajaprov").innerHTML=visible;
+}
+function provincias(){
+    var  provi= document.getElementById("prov").value;
+    var tabl="";
+    tabl += "<p> tabla con usuarios:</p> <table style ='border:1px solid black;'>";
+    tabl +="<tr>"+
+    "<td>"+
+    "Cliente"+
+    "</td>"+
+    "<td>"+
+    "Localidad"+
+   " </td>"+
+    "<td>"+
+    "Cuota"+
+    "</td>"+
+    "</tr>";
+    arraylimpio.forEach(element => {
+       if (provi==element[1]) {
+        tabl+="<tr>";
+        for (let index = 0; index < element.length; index++) {
+            tabl+="<td>"+element[index]+"</td>"   
+        }
+        tabl+="</tr>";
+       } 
+       
+    });
+
+    tabl+="</table>";
+    document.getElementById("usuario").innerHTML=tabl;
+
+    /*for (let index = 0; index < arraylimpio.length; index++) {
+        
+        if(provi==arraylimpio[1]){
+            alert("dentro del if");
+        tabl+="<tr>";
+        for (let i = 0; i < arraylimpio[index].length; i++) {
+            tabl+="<td>"+arraylimpio[index][i]+"</td>";  
+        }
+        tabl+="</tr>";
+     }
+    }
+    tabl+="</table>";
+    document.getElementById("usuario").innerHTML=tabl;*/
+
+    
 }
 
--- Unidad3/String ej3/string1.js
+++ Unidad3/String ej3/string1.js
@@ -14,5 +14,18 @@ function escribenombre(){
     document.writeln("Tu nombre es " + arrayc[0]+"<br>");
     document.writeln("Tu primer apellido es "+arrayc[1]+"<br>");
     document.writeln( "Tu  segundo apellidos es "+arrayc[2]+"<br>");
+
+    var propuesta1= arrayc[0][0].concat(arrayc[1]).concat(arrayc[2][0]);
+    document.writeln("Propuesta de nombre 1:"+propuesta1+"<br>");
+    var propuesta2="";
+    
+    for (let index = 0; index < arrayc.length; index++) {
+        for (let i= 0; i < 3; i++) {
+           propuesta2+=arrayc[index][i];
+            
+        }
+        
+    }
+    document.writeln("Propuesta de nombre 2:"+propuesta2);
 }
 
