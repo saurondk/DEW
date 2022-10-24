8f63690d5bb5974a39de12a566842eece8a30c4b
2DAW12 <2DAW12@DOMDPTOINFJMPP.INF>
2022-10-24 19:53:24 +0200

    array parte 2

--- Unidad4/ej1/js/array.js
+++ Unidad4/ej1/js/array.js
@@ -2,49 +2,58 @@ var paises = ["España","Alemania","Chile","Malasia","Iran","Venezuela","Italia"
 
 
 function arrayNumero(arraypaises){
-    var paises2=[];
-    paises2=(arraypaises+"").split(",");
-    var numero = paises2.length;
-    document.getElementById("salida").innerHTML= "El numero de elementos del array es "+ numero +"<br>";
+
+    var numero = arraypaises.length;
+    document.getElementById("salida").innerHTML+= "<p><h3>Elementos del array</h3></p> El numero de elementos del array es "+ numero +"<br>";
 }
 
 function arrayMuestra(arraypaises) {
-    var paises2=[];
-    paises2=(arraypaises+"").split(",");
-    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+    
+    document.getElementById("salida").innerHTML+= "<p><h3>Array a String</h3></p>"+arraypaises.toString()+"<br>";
 
 }
+
 function arrayInverso(arraypaises) {
-    var paises2=[];
-    paises2=(arraypaises+"").split(",");
-    paises2.reverse();
-    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+    var arraylocal=arraypaises;
+    arraylocal.reverse();
+    document.getElementById("salida").innerHTML+="<p><h3>Array Inverso</h3></p>"+ arraylocal.toString()+"<br>";
 }
+
 function arrayOrdenAlfa(arraypaises){
-var paises2=[];
-paises2=(arraypaises+"").split(",");
-paises2.sort();
-document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+    var arraylocal=arraypaises;
+arraylocal.sort();
+document.getElementById("salida").innerHTML+= "<p><h3>Array ordenados alfabeticamente</h3></p>"+arraylocal.toString()+"<br>";
 }
+
 function arrayInsertaPrincipio(arraypaises){
-    var paises2=[];
-    paises2=(arraypaises+"").split(",");
-    paises2.unshift("Estonia");
-    document.getElementById("salida").innerHTML+= paises2[0]+"<br>";
-    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+    
+    arraypaises.unshift("Estonia");
+    document.getElementById("salida").innerHTML+="<p><h3> Insertando un elementos  el array</h3></p>"+ arraypaises[0]+"<br>";
+    document.getElementById("salida").innerHTML+= arraypaises.toString()+"<br>";
 }
 //Borrar un elemento al principio del array (y decir cuál se ha borrado).
 function arrayBorrarPrincipio(arraypaises) {
-    var paises2=[];
-    paises2=(arraypaises+"").split(",");
-    document.getElementById("salida").innerHTML+= paises2.shift()+"<br>";
-    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+
+    document.getElementById("salida").innerHTML+="<p><h3>Borrando un elemento al principio del array</h3></p>"+ arraypaises.shift()+"<br>";
+    document.getElementById("salida").innerHTML+= arraypaises.toString()+"<br>";
 }
 //Borrar un elemento al final del array (y decir cuál se ha borrado).
 
 function arrayBorrarFinal(arraypaises) {
-    var paises2=[];
-    paises2=(arraypaises+"").split(",");
-    document.getElementById("salida").innerHTML+= paises2.pop()+"<br>";
-    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+    
+    document.getElementById("salida").innerHTML+="<p><h3>Borrando al final del array</h3></p>"+ arraypaises.pop()+"<br>";
+    arrayMuestra(paises);
+
+}
+function mostrarElemento(arraypaises) {
+
+    var elemento =prompt("Introduce una posición del array para ver el elemento del 0 al "+arraypaises.length);
+    document.getElementById("salida").innerHTML+="<p><h3> El elemento del array es </h3></p>"+arraypaises[elemento];
+    
+}
+//Muestra la posición en la que se encuentra un elemento que le indica el usuario.
+function mostrarPosicion(arraypaises) {
+
+    var elemento =prompt("Introduce el nombre del elemento para ver su posición en el array");
+    document.getElementById("salida").innerHTML+="<p><h3>La posición del elemento es </h3></p>"+arraypaises.indexOf(elemento)+"<br>";
 }
