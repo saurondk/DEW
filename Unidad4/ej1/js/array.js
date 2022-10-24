4f0b185632c4e11db82014486db32003be350bb9
saurondk <93879292+saurondk@users.noreply.github.com>
2022-10-24 12:22:07 +0100

    arrays 1 parte continuacion

--- Unidad4/ej1/js/array.js
+++ Unidad4/ej1/js/array.js
@@ -27,6 +27,24 @@ paises2.sort();
 document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
 }
 function arrayInsertaPrincipio(arraypaises){
+    var paises2=[];
+    paises2=(arraypaises+"").split(",");
+    paises2.unshift("Estonia");
+    document.getElementById("salida").innerHTML+= paises2[0]+"<br>";
+    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+}
+//Borrar un elemento al principio del array (y decir cuál se ha borrado).
+function arrayBorrarPrincipio(arraypaises) {
+    var paises2=[];
+    paises2=(arraypaises+"").split(",");
+    document.getElementById("salida").innerHTML+= paises2.shift()+"<br>";
+    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
+}
+//Borrar un elemento al final del array (y decir cuál se ha borrado).
 
-    
+function arrayBorrarFinal(arraypaises) {
+    var paises2=[];
+    paises2=(arraypaises+"").split(",");
+    document.getElementById("salida").innerHTML+= paises2.pop()+"<br>";
+    document.getElementById("salida").innerHTML+= paises2.toString()+"<br>";
 }
