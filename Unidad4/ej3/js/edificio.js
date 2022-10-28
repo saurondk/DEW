d958f0713e91126f099a2cf6d335031cd1231378
saurondk <93879292+saurondk@users.noreply.github.com>
2022-10-28 20:54:35 +0100

    part3

--- Unidad4/ej3/js/edificio.js
+++ Unidad4/ej3/js/edificio.js
@@ -1,7 +1,5 @@
 class Edificio {
     plantas = [];
-    
-
     constructor(calle, numero, cp) {
         this.calle = calle;
         this.numero = numero;
@@ -11,21 +9,21 @@ class Edificio {
     //metodo agregarPlantasYPuertas
 
     agregarPlantasYPuertas(numPlantas, numPuertas) {
-       var puertas=[];
-        
+        var puertas = [];
+
         for (let j = 1; j <= numPuertas; j++) {
-            puertas.push([""]);
+            puertas.push([]);
         }
 
         for (let i = 1; i <= numPlantas; i++) {
-            this.plantas.push(Object.assign({},puertas));
+            this.plantas.push(Object.assign({}, puertas));
         }
     }
 
-
     agregarPropietario(nombre, planta, puerta) {
-      
-        this.plantas[planta][puerta]=nombre;
+
+        this.plantas[planta][puerta] = nombre;
+        document.getElementById("salida").innerHTML += nombre+" es ahora propietario del piso  " + planta+" Puerta "+puerta+ "<br>";
 
     }
     modificarNumero(numero) {
@@ -38,16 +36,16 @@ class Edificio {
         this.cp = cp;
     }
     imprimecalle() {
-        document.getElementById("salida").innerHTML = "La calle es " + calle + "<br>";
+        document.getElementById("salida").innerHTML += "La calle es " + this.calle + "<br>";
     }
     imprimenumero() {
-        document.getElementById("salida").innerHTML = "El numero es " + numero + "<br>";
+        document.getElementById("salida").innerHTML += "El numero es " + this.numero + "<br>";
     }
     imprimeCp() {
-        document.getElementById("salida").innerHTML = "El codigo postal es " + cp + "<br>";
+        document.getElementById("salida").innerHTML += "El codigo postal es " + this.cp + "<br>";
     }
     mensajeConstruir() {
-        document.getElementById("salida").innerHTML = "Nuevo edificio construido en la calle " + this.calle + " nº " + this.numero + " CP: " + this.cp + "<br>";
+        document.getElementById("salida").innerHTML += "Nuevo edificio construido en la calle " + this.calle + " nº " + this.numero + " CP: " + this.cp + "<br>";
 
     }
 
@@ -62,57 +60,66 @@ window.crearEdificio = function crearEdificio() {
     var edificio = new Edificio(calle, numero, cp);
     edificios.push(edificio);
     console.log(edificios);
-
 }
-window.verDatosEdificio =function verDatosEdificio() {
-    var numeroi = prompt("Introduce numero");
-    edificios.forEach(element => {
-        if (element.numero == numeroi) {
-            plantas.forEach(element => {
+
+window.verDatosEdificio = function verDatosEdificio() {
+    var edificionum = prompt("Introduce numero de edificio");
+
+    for (let i = 0; i < edificios.length; i++) {
+        if (edificionum == edificios[i].numero) {
+
+            edificios[i].imprimecalle();
+            edificios[i].imprimenumero();
+            edificios[i].imprimeCp();
+            //var plantas=0
+            for (let j = 0; j < edificios[i].plantas.length; j++) {
+                var k=0;
+                edificios[i][j].forEach(element => {
+                    document.getElementById("salida").innerHTML+="Propietario del piso"+k+" de la planta "+j+" es "+element;
+                    k++;
+                });
+                    
+                    
+                    
                 
-            });
+                
+                
+            }
+            
         }
 
-    });
-
+    }
 }
-/*window.probar=function probar(){
-   var  edificio1= new Edificio("1","2","3");
-   edificio1.agregarPlantasYPuertas(2,2);
-   edificio1.agregarPropietario("aram",1,0);
-   edificio1.agregarPropietario("diego,",1,1);
-   document.getElementById("salida").innerHTML=edificio1.plantas[1][0];
-   console.log(edificio1);
-}*/
 
-window.probar =function insertePropietarios() {
+
+window.insertePropietarios = function insertePropietarios() {
     var edificionum = prompt("Introduce numero de edificio");
     
     for (let i = 0; i < edificios.length; i++) {
-        if (edificionum==edificios[i]) {
+        if (edificionum == edificios[i].numero) {
             var planta = prompt("Introduce una planta");
-            var puerta =prompt("Introduce una puerta");
-            var nombre =prompt ("Introduce un nombre");
-            element.agregarPropietario(nombre,planta,puerta);
+            var puerta = prompt("Introduce una puerta");
+            var nombre = prompt("Introduce un nombre");
+            console.log(edificios[i]);
+            edificios[i].agregarPropietario(nombre, planta, puerta);
         }
-        
+
+
     }
-window.plantasYPuertas= function plantasYPuertas(){
-           var planta = prompt("Introduce una planta");
-            var puerta =prompt("Introduce una puerta");
-            var edificio=prompt("Introduce el edificio"+edificios.length);
-            edificio.agregarPlantasYPuertas(planta,puerta);
-
-}   
-    /*edificios.forEach(element => {
-        if (edificionum==element[1]) {
-            var planta = prompt("Introduce una planta");
-            var puerta =prompt("Introduce una puerta");
-            var nombre =prompt ("Introduce un nombre");
-            element.agregarPropietario(nombre,planta,puerta);
-        }
-    });*/
-
+    console.log(edificios);
 }
+window.plantasYPuertas = function plantasYPuertas() {
+    var edificionum = prompt("Introduce numero de edificio");
+    for (let i = 0; i < edificios.length; i++) {
+        if (edificionum == edificios[i].numero) {
+            var planta = prompt("Introduce una planta");
+            var puerta = prompt("Introduce una puerta");
+            edificios[i].agregarPlantasYPuertas(planta, puerta);
+
+        }
+    }
+    console.log(edificios);
+}
+
 
 
