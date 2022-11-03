ae54fd08a2707a559aa450dce4b55a6972e4ca01
2DAW12 <2DAW12@DOMDPTOINFJMPP.INF>
2022-11-03 19:49:59 +0100

    edificio terminado

--- Unidad4/ej3/js/edificio.js
+++ Unidad4/ej3/js/edificio.js
@@ -9,21 +9,27 @@ class Edificio {
     //metodo agregarPlantasYPuertas
 
     agregarPlantasYPuertas(numPlantas, numPuertas) {
+    
+        for (let i = 1; i <= numPlantas; i++) {
+           var puertas=this.crearP(numPuertas);
+            //this.plantas.push(Object.assign({}, puertas));
+            this.plantas.push(puertas);
+        }
+    }
+
+    crearP(nump){
         var puertas = [];
 
-        for (let j = 1; j <= numPuertas; j++) {
-            puertas.push([]);
+        for (let j = 1; j <= nump; j++) {
+            puertas.push(["piso vacio"]);
         }
-
-        for (let i = 1; i <= numPlantas; i++) {
-            this.plantas.push(Object.assign({}, puertas));
-        }
+        return puertas;
     }
 
     agregarPropietario(nombre, planta, puerta) {
 
         this.plantas[planta][puerta] = nombre;
-        document.getElementById("salida").innerHTML += nombre+" es ahora propietario del piso  " + planta+" Puerta "+puerta+ "<br>";
+        document.getElementById("salida").innerHTML += nombre+" es ahora propietario del piso  " + (planta+1)+" Puerta "+(puerta+1)+ "<br>";
 
     }
     modificarNumero(numero) {
@@ -71,21 +77,22 @@ window.verDatosEdificio = function verDatosEdificio() {
             edificios[i].imprimecalle();
             edificios[i].imprimenumero();
             edificios[i].imprimeCp();
-            //console.log(edificios[i].plantas);
+            console.log(edificios[i].plantas[0]);
+            console.log(typeof(edificios[i].plantas[0]));
             for (let j = 0; j < edificios[i].plantas.length; j++) {
                 console.log( edificios[i].plantas[j]);
                 console.log( edificios[i].plantas[j].length);
                 console.log( edificios[i].plantas[j][0] );
                 //no me obtiene la ultima posicion del array linea 78 obtiene el nombre en esa posicion pero no funciona el .length del bucle for
-                for (let k = 0; k <= edificios[i].plantas[j].length; k++) {
+                for (let k = 0; k < edificios[i].plantas[j].length; k++) {
                     console.log( edificios[i].plantas[j][k] );
-                    document.getElementById("salida").innerHTML+="Propietario del piso"+k+" de la planta "+j+" es "+edificios[i].plantas[j][k];
+                    document.getElementById("salida").innerHTML+="Propietario del piso "+(parseInt(k,10)+1)+" de la puerta "+(parseInt(j,10)+1)+" es "+edificios[i].plantas[j][k]+"<br>";
 
-                }    
-                edificios[i].plantas[j].forEach(element => {
+                } 
+                /*edificios[i].plantas[j].forEach(element => {
                     console.log(element);
                     document.getElementById("salida").innerHTML+="Propietario del piso"+k+" de la planta "+j+" es "+element;
-                });
+                });*/
             }
             
         }
@@ -123,5 +130,27 @@ window.plantasYPuertas = function plantasYPuertas() {
     console.log(edificios);
 }
 
+/*Instanciamos 3 objetos edificioA, edificioB y edificioC con estos datos:
+
+Construido nuevo edificio en calle: Garcia Prieto, nº: 58, CP: 15706.
+Construido nuevo edificio en calle: Camino Caneiro, nº: 29, CP: 32004.
+Construido nuevo edificio en calle: San Clemente, nº: s/n, CP: 15705.
+El código postal del edificio A es: 15706.
+La calle del edificio C es: San Clemente.
+El edificio B está situado en la calle Camino Caneiro número 29.*/
+
+
+window.cargardatos= function cargardatos() {
+    var edificioA = new Edificio('Garcia Prieto', '58', '15706');
+    var edificioB = new Edificio('Camino Caneriro', '29', '32004');
+    var edificioC = new Edificio('San Clemente', 's/n', '15705');
+    edificios.push(edificioA,edificioB,edificioC);
+    //Agregamos 2 plantas y 3 puertas por planta al edificio A...
+    edificioA.agregarPlantasYPuertas(3,2);
+    edificioA.agregarPropietario('Jose antonio Lopez',0,0);
+    edificioA.agregarPropietario('Luisa Martin',0,1);
+    edificioA.agregarPropietario('Marta castellon',0,2);
+    edificioA.agregarPropietario('Antonio Pereira',1,1);
+}
 
 
