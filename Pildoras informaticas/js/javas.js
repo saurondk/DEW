74cfd48760dd8d9b944e13a764e5c3d25e44e1a1
saurondk <93879292+saurondk@users.noreply.github.com>
2022-11-21 13:37:04 +0000

    casa

--- Pildoras informaticas/js/javas.js
+++ Pildoras informaticas/js/javas.js
@@ -1,2 +1,18 @@
-var colores = ["verde", "azul", "rojo"];
-console.log(colores[0]);
+var numeros = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+
+var total=0;
+for (let index = 0; index < numeros.length; index++) {
+    
+    
+
+
+    if(index%2==0){
+        console.log(index);
+    }
+    total+=numeros[index];
+    
+
+    
+    
+}
+console.log(total);
