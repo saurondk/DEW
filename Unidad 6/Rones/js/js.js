eb361811ccf8c5e2a4382956fac3386f6acc23b7
2DAW12 <2DAW12@DOMDPTOINFJMPP.INF>
2023-01-10 16:42:08 +0100

    rones v2

--- Unidad 6/Rones/js/js.js
+++ Unidad 6/Rones/js/js.js
@@ -15,16 +15,14 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Rum')
         data.drinks.forEach( element => {
             var imagen = element.strDrinkThumb;
             var titulo = element.strDrink;
-            respuesta.innerHTML += `<div class="card" style="width: 18rem;"> 
-            <img src="${imagen}" class="card-img-top" alt="...">
+            respuesta.innerHTML += `<div class="card mb-3 me-3" style="width: 18rem;"> 
+            <img src="${imagen}" class="card-img-top" alt="${titulo}">
             <div class="card-body"><h5 class="card-title">${titulo}</h5>
             <p class="card-text">${element.idDrink}</p>
-            <a href="#"  id="${element.idDrink}" class="btn btn-primary">Añadir</a>
+            <a href="#"  id="${element.idDrink}" class="btn btn-primary">Pedir</a>
             </div>
             </div>`
-           
 
-        
            
         });
        
@@ -39,7 +37,36 @@ function enviar(data) {
          
         botonid.addEventListener('click', function(){
                 arrayp.push(element.idDrink);
+                setCookie("pedido",arrayp,7); //añadimos la cookie una vez ya hemos creado los set get y check.
                 console.log(arrayp);
             });
         });
 }
+
+// seteamos la cookies
+function setCookie(pedido, value, expiry) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expiry * 24 * 60 * 60 * 1000));
+    var expires = "expires="+date.toUTCString();
+    document.cookie = pedido + "=" + value + ";" + expires + ";";
+  }
+
+  function getCookie(pedido) {
+    return checkCookie(pedido);
+  }
+  function checkCookie(pedido) {
+    let name = pedido + "=";
+    let spli = document.cookie.split(';');
+    for(var j = 0; j < spli.length; j++) {
+      let char = spli[j];
+      while (char.charAt(0) == ' ') {
+        char = char.substring(1);
+      }
+      if (char.indexOf(name) == 0) {
+        return char.substring(name.length, char.length);
+      }
+    }
+    return "";
+  }
+  
+  checkCookie();
