95c1acf52e5fe385db4ca62bb536826df8cb0b89
saurondk <93879292+saurondk@users.noreply.github.com>
2023-02-09 12:08:51 +0000

    chuck

--- exam3eva/practica3/js.js
+++ exam3eva/practica3/js.js
@@ -1,12 +1,18 @@
 const container = document.querySelector('.container');
-const buscarInput = document.querySelector('#buscarInput');
+
+const buscarInput = document.querySelector('#buscarInput');///filtrar
 const buscarBoton = document.querySelector('#buscarBoton');
+
+const busqueda = document.querySelector('#busqueda');// otro fetch
+const busquedaBoton = document.querySelector('#busquedaBoton');
 
 window.addEventListener('DOMContentLoaded',async()=>{
 
      const datos = await fecthDatos();
     console.log(datos)
     Categoria(datos);
+    Buscar(datos);
+    
 })
 
 async function fecthDatos (){
@@ -15,6 +21,28 @@ async function fecthDatos (){
     return await response.json();
 }
 
+
+
+//otro fetch
+    busquedaBoton.addEventListener('click', async ()=>{ 
+    
+        const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${busqueda.value}`);
+        const datos = await response.json();
+       console.log(datos);
+       click(datos);
+        
+    });
+
+    function click(datos){
+        
+        datos.result.forEach(element => {
+        const p = document.createElement('p');
+        p.textContent=element.value;
+        container.appendChild(p);
+
+        });
+    }
+
 function Categoria(datos) {
     datos.forEach(element => {
         const p = document.createElement('p');
@@ -22,10 +50,20 @@ function Categoria(datos) {
         container.appendChild(p);
     });
 }
+
+
 function Buscar(datos) {
     buscarBoton.addEventListener('click',()=>{
         const filtrados = datos.filter(element => element == buscarInput.value);
+        console.log(buscarInput.value)
+        borrarNodo(container);
         Categoria(filtrados);
     })
 }
-function borrarNodo
+
+
+function borrarNodo(nodo) {
+    while (nodo.firstChild) {
+        nodo.removeChild(nodo.firstChild);
+    }
+}
