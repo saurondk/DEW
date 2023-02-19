eeed3c54516723e52c8ab220a6272460cd6c03e5
saurondk <93879292+saurondk@users.noreply.github.com>
2023-02-19 19:19:41 +0000

    examen2

--- exam3eva/harry2/js.js
+++ exam3eva/harry2/js.js
@@ -7,9 +7,10 @@ const inputBuscar =document.querySelector('.inputBuscar');
 window.addEventListener('DOMContentLoaded', async()=>{
 
 const datos =  await Datos();
+console.log(datos);
 crearTarjeta(datos);
 Filtrar(datos);
-
+seleccion(datos);
 });
 
  async function  Datos() {
@@ -62,5 +63,29 @@ function borrarNodo(nodo) {
     while (nodo.firstChild) {
         nodo.removeChild(nodo.firstChild);
     }
+}
+
+const select = document.querySelector('#genero');
+
+function seleccion(datos) {
+    let valores = ['male', 'female'];
+
+    const opt1 = document.createElement('option');
+    const opt2 = document.createElement('option');
+
+    opt1.setAttribute('value', valores[0]);
+    opt2.setAttribute('value', valores[1]);
+    opt1.textContent = 'Masculino';
+    opt2.textContent = 'Femenino';
+
+    select.appendChild(opt1);
+    select.appendChild(opt2);
+
+    select.addEventListener('change', () => {
+        const filtrados = datos.filter(element => element.gender == select.value);
+        console.log(filtrados);
+        borrarNodo(container);
+        crearTarjeta(filtrados);
+    });
 }
 
